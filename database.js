e173ea6a496702612c8721fefbe2234c98e489d5
Goldy Mariz Lunesa <gmlunesa@gmail.com>
2017-10-02 10:33:45 +0800

    Updated comments

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 
-const connectionUrl = 'postgres://study:study@localhost:5432/study';// sayup pa ni
+// connect to local database
+const connectionUrl = 'postgres://study:study@localhost:5432/study'; 
 const database = new Sequelize(connectionUrl);
 
 module.exports = database;
