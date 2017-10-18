373823545ab8ab4da9cfd555bd0f686a33681a63
Brettny Onez <brettny.onez@gmail.com>
2017-10-18 19:05:12 +0800

    Added spaces

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
-const Sequelize = require('sequelize');
+const Sequelize = require( 'sequelize' );
 
 // connect to local database
 const connectionUrl = 'postgres://study:study@localhost:5432/study'; 
-const database = new Sequelize(connectionUrl);
+const database = new Sequelize( connectionUrl );
 
 module.exports = database;
 
